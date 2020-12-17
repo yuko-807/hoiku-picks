1e66132b520e81d863f537aade73e22435fddce3
MaQueen <shusineko@icloud.com>
2020-12-17 10:36:29 +0900

    commit

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+import styles from "../styles/global.module.scss";
+import Header from '../components/header/header';
+
+export default function Home() {
+  return (
+    <>
+    <Header/>
+    </>
+  );
+}
