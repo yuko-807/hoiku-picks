c637847263f5923252720c83d0cd3ab6faf613df
MaQueen <shusineko@icloud.com>
2020-12-18 15:28:26 +0900

    commit

---
+++ pages/others/company.tsx
@@ -0,0 +1,11 @@
+import { Layout } from '../../components/layout';
+import { Company } from '../../components/companyContent/company'
+
+export default function User():JSX.Element{
+  return(
+    <>
+      <Layout/>
+      <Company/>
+    </>
+  );
+};
