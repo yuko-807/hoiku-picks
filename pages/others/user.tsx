2abfefab6960ea9ca37c59d15210730ccb41eaed
MaQueen <shusineko@icloud.com>
2020-12-18 11:58:33 +0900

    commit

--- pages/others/user.tsx
+++ pages/others/user.tsx
@@ -1,9 +1,11 @@
 import { Layout } from '../../components/layout';
+import { Main } from '../../components/userMainContent/main'
 
-export default function User(){
+export default function User():JSX.Element{
   return(
     <>
       <Layout/>
+      <Main/>
     </>
   );
 };
