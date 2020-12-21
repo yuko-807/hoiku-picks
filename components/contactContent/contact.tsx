d32fb3d48216407787c1020b68eb2d58158e674a
MaQueen <shusineko@icloud.com>
2020-12-21 18:21:25 +0900

    commit

--- components/contactContent/contact.tsx
+++ components/contactContent/contact.tsx
@@ -5,10 +5,9 @@ export function Contact(): JSX.Element{
   return(
     <>
     <div className={styles.form}>
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSehu469O1Vu150AGskiSAM8S-Ydyj0Y8iUVPjaitemaatIavQ/viewform?embedded=true" width="100%" height="900" >読み込んでいます…</iframe>
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScucxVEKqGQptDvTEruOULvHirSrVIsTssSAbZRrZGVkdBZUA/viewform?embedded=true" width="100%" height="900" >読み込んでいます…</iframe>
       </div>
     </>
   )
 }
-
 export default Contact;
