69ea90db03f090e1e630e6fb8a1d9294328defbf
MaQueen <shusineko@icloud.com>
2020-12-21 19:11:17 +0900

    commit

--- components/contactContent/contact.tsx
+++ components/contactContent/contact.tsx
@@ -4,8 +4,8 @@ import styles from './index.module.css'
 export function Contact(): JSX.Element{
   return(
     <>
-    <div className={styles.form}>
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScucxVEKqGQptDvTEruOULvHirSrVIsTssSAbZRrZGVkdBZUA/viewform?embedded=true" width="100%" height="900" >読み込んでいます…</iframe>
+      <div className={styles.form}>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScucxVEKqGQptDvTEruOULvHirSrVIsTssSAbZRrZGVkdBZUA/viewform?embedded=true" width="100%" height="900" >読み込んでいます…</iframe>
       </div>
     </>
   )
