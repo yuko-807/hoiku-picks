19f93b797242f931df769e832dede8f967fc7cb4
MaQueen <shusineko@icloud.com>
2020-12-22 15:51:47 +0900

    commit

--- components/userMainContent/main.tsx
+++ components/userMainContent/main.tsx
@@ -7,7 +7,9 @@ export function Main(): JSX.Element{
       <div className={styles.mainContent}>
         <div className={styles.mainContentText}>
           <h1>
-            単発・長期で働ける！<br />保育士専用<br />お仕事マッチングアプリ
+            保育園で働くなら<br/><img className={styles.logo} src="/img/hoiku-logo.png" alt="logo"/>
+            {/* スキマ時間で自分に合った<br/>保育園で働ける<br/>保育園マッチングアプリ */}
+            {/* 単発・長期で働ける！<br />保育士専用<br />お仕事マッチングアプリ */}
           </h1>
         </div>
         <div className={styles.appContents}>
