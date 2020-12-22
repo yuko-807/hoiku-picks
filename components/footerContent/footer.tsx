95a87b69366f6a9461d800dab28d6f87c6e91b37
MaQueen <shusineko@icloud.com>
2020-12-22 16:48:05 +0900

    rootをユーザーページに変更/clientに変更

--- components/footerContent/footer.tsx
+++ components/footerContent/footer.tsx
@@ -8,18 +8,18 @@ export function Footer(): JSX.Element{
     <footer className={styles.footer}>
       <div className={styles.footerContent}>
         <ul>
-          <li><Link href="/"><a>事業者の方はこちら</a></Link></li>
-          <li><Link href="/others/user"><a>ユーザーの方はこちら</a></Link></li>
+          <li><Link href="/"><a>ユーザーの方はこちら</a></Link></li>
+          <li><Link href="/client"><a>事業者の方はこちら</a></Link></li>
           <li><Link href="/others/company"><a>会社概要</a></Link></li>
           <li><Link href="/others/faq"><a>よくある質問</a></Link></li>
           <li><Link href="/others/conect"><a>お問い合わせ</a></Link></li>
           <li><Link href="/others/team"><a>利用規約</a></Link></li>
         </ul>
-        <div className={styles.socialIcons}>
+        {/* <div className={styles.socialIcons}>
           <Link href="#"><a><img src="" alt=""/></a></Link>
           <Link href="#"><a></a></Link>
           <Link href="#"><a></a></Link>
-        </div>
+        </div> */}
       </div>
       <div className="ARR">
         <small>©Gouth, Inc. All right reserved.</small>
