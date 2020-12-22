aef4b7fc06dbb6c62e294841f2129bda0a040d15
MaQueen <shusineko@icloud.com>
2020-12-22 16:32:09 +0900

    commit

--- components/userWorkContent/data.tsx
+++ components/userWorkContent/data.tsx
@@ -1,21 +1,21 @@
 export const works = [
   {
     id: 1,
-    title: 'POINT1',
+    title: 'STEP1',
     image: '/img/user-ss-w3.svg',
     sub: 'お仕事を探す！',
     text: '一覧となって出てくるので、気になった求人があったら詳細をみましょう！',
   },
   {
     id: 2,
-    title: 'POINT2',
+    title: 'STEP2',
     image: '/img/user-ss-w2.svg',
     sub: '求人に申請！',
     text: '一覧から気になった求人詳細をみて、実際に申請しましょう。',
   },
   {
     id: 3,
-    title: 'POINT3',
+    title: 'STEP3',
     image: '/img/user-ss-w1.svg',
     sub: '承諾されたらコンタクト！',
     text: '申請が承諾されると、保育園側とコンタクトが取れます。不安に思っていることや、疑問点をここで解消しましょう！',
