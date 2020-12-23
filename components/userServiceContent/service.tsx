c088391e746a6f879a11237a94adee0a91353b7d
MaQueen <shusineko@icloud.com>
2020-12-23 11:07:34 +0900

    commit

--- components/userServiceContent/service.tsx
+++ components/userServiceContent/service.tsx
@@ -3,7 +3,7 @@ import styles from './index.module.css'
 export function Service(): JSX.Element{
   return(
     <div className={styles.serviceContents}>
-      <h1 className={styles.contentTitle}>保育Picksってどんなサービス??</h1>
+      <p className={styles.contentTitle}>保育Picksってどんなサービス??</p>
       <div className={styles.serviceContent}>
         <div className={styles.serviceContentText}>
           <img
