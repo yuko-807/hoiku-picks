dfee0bc5458a22d9f84eb98c4af4562a504083bf
MaQueen <shusineko@icloud.com>
2020-12-24 18:57:32 +0900

    commit

--- components/mainContent/main.tsx
+++ components/mainContent/main.tsx
@@ -8,8 +8,7 @@ export function mainContent():JSX.Element{
       <div className={styles.mainContent}>
         <div className={styles.mainText}>
           <h1>「その日」「その時間」<br />の保育士不足を無くす。</h1>
-          <p>1日単位の採用から繋がる</p>
-          <p>保育士採用マッチングアプリ</p>
+          <p>1日単位の採用から繋がる<br/>保育士採用マッチングアプリ</p>
         </div>
         <div className={styles.mainLinks}>
           <Link href="/others/sorry">
--- components/pointContent/data.tsx
+++ components/pointContent/data.tsx
@@ -2,30 +2,30 @@ export const points = [
   {
     id: 1,
     title: 'POINT1',
-    image: '/img/undraw_children_4rtb.svg',
-    sub: '安心の労務面',
-    text: '保育士資格を持っているワーカーのみ登録しているので、即戦力になります。',
-  },
-  {
-    id: 2,
-    title: 'POINT2',
-    image: '/img/undraw_Messages_re_qy9x.svg',
-    sub: '簡単な操作で募集が出せる',
-    text: '簡単3ステップで、すぐに応募を出せます。',
-  },
-  {
-    id: 3,
-    title: 'POINT3',
     image: '/img/undraw_Savings_re_eq4w.svg',
     sub: '導入・運用コスト完全無料',
     text: '登録料金は完全無料なのでお気軽に始められます。',
   },
   {
-    id: 4,
-    title: 'POINT4',
+    id: 2,
+    title: 'POINT2',
     image: '/img/undraw_travel_together_re_kjf2.svg',
     sub: '最短1分でワーカーとマッチング',
     text: '募集後、すぐに保育士ワーカーのもとに情報公開されるのでマッチングもスピーディ。',
+  },
+  {
+    id: 3,
+    title: 'POINT3',
+    image: '/img/undraw_Messages_re_qy9x.svg',
+    sub: '簡単な操作で募集が出せる',
+    text: '簡単3ステップで、すぐに応募を出せます。',
+  },
+  {
+    id: 4,
+    title: 'POINT4',
+    image: '/img/undraw_children_4rtb.svg',
+    sub: '安心の労務面',
+    text: '保育士資格を持っているワーカーのみ登録しているので、即戦力になります。',
   },
   
 ];
